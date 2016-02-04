21fcfd7b271a2680d5c9b3a4aaf34f616ed377d4
SunRain <41245110@qq.com>
2016-02-04 10:41:28 +0800

    move htmlcxx to signal git repo

--- QWeiboSDK/HackLogin/htmlcxx/css/css_lex.h
+++
@@ -1,7 +0,0 @@
-#ifndef __CSS_LEX_H__
-#define __CSS_LEX_H__
-
-int init_yylex(const char *buffer, int buf_len);
-int end_yylex();
-
-#endif
--- QWeiboSDK/HackLogin/htmlcxx/html/ParserSax.cc
+++
@@ -1,7 +0,0 @@
-#include "ParserSax.h"
-
-void htmlcxx::HTML::ParserSax::parse(const std::string &html)
-{
-//	std::cerr << "Parsing string" << std::endl;
-	parse(html.c_str(), html.c_str() + html.length());
-}
